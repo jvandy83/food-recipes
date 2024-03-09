f4eb4fc8be162b9b8c1069e8b089c67ca06874c3
jvandy83 <vanthedev@gmail.com>
2024-03-08 23:52:35 -0700

    added wait_for_db to dockerfile and gh actions

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,14 +1,7 @@
-"""Django command to wait for the database to be available"""
-
 import time
-
-
 from psycopg2 import OperationalError as Psycopg2Error
-
-
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-
 
 class Command(BaseCommand):
     """Django command to wait for database."""
@@ -16,13 +9,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
-        db_up = False
-        while db_up is False:
+        max_retries = 10
+        retries = 0
+        
+        while retries < max_retries:
             try:
                 self.check(databases=['default'])
-                db_up = True
+                self.stdout.write(self.style.SUCCESS('Database available!'))
+                return  # Exit the function once the database is available
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
+                retries += 1
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.ERROR('Database still unavailable after retries.'))
